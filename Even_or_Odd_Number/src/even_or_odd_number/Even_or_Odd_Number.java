6b4735ef522609962c51fdcb11f237e7c5d64675
jesumike <jesumike@DESKTOP-AUH1J82>
2026-08-16 13:11:39 +0800

    Completion of Odd or Even number

--- Even_or_Odd_Number/src/even_or_odd_number/Even_or_Odd_Number.java
+++ Even_or_Odd_Number/src/even_or_odd_number/Even_or_Odd_Number.java
@@ -4,6 +4,8 @@
  */
 package even_or_odd_number;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jesumike
@@ -14,7 +16,15 @@ public class Even_or_Odd_Number {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the an integer");
+        int number = input.nextInt();
+        if (number % 2==0){
+        System.out.println("The number is even.");
+     } else {
+            System.out.println("The number is odd");
+       
+}   
     }
-    
 }
+
