cc3f0188cdea60d344ed38d2f4594b96873cb6a6
jesumike <jesumike@DESKTOP-AUH1J82>
2026-08-16 12:56:40 +0800

    initial commit

---
+++ Even_or_Odd_Number/src/even_or_odd_number/Even_or_Odd_Number.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package even_or_odd_number;
+
+/**
+ *
+ * @author jesumike
+ */
+public class Even_or_Odd_Number {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
